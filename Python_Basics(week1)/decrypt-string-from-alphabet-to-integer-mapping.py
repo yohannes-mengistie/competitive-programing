5d7c25bd6e41a5f3cfb9ebea3b68f3bf666952e5
yohannes-mengistie <120666489+yohannes-mengistie@users.noreply.github.com>
2023-12-03 21:37:11 +0300

    Added solution for Decrypt String from Alphabet to Integer Mapping

---
+++ Python_Basics(week1)/decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,22 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        dic1 = {'1':"a",'2':"b",'3':"c",'4':"d",'5':"e",'6':"f",'7':"g",'8':"h",'9':"i"}
+        dic2 = {'10#':'j','11#':'k','12#':'l','13#':'m','14#':'n','15#':'o','16#':'p','17#':'q','18#':'r','19#':'s','20#':'t','21#':'u','22#':'v','23#':'w','24#':'x','25#':'y','26#':'z',}
+        ans = []
+        s = s[::-1]
+        i = 0
+        while i < len(s):
+            if s[i] == "#":
+                cur = s[i:i+3][::-1]
+                ans.append(dic2[cur])
+                i +=3
+            else:
+                ans.append(dic1[s[i]])
+            
+                i +=1
+        ans = ans[::-1]
+        
+        return "".join(ans)
+            
+            
+                
