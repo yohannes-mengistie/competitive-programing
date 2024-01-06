de62ecc2c3238bdc8421dac3139cdea2eec4396d
yohannes-mengistie <120666489+yohannes-mengistie@users.noreply.github.com>
2024-01-06 15:44:52 +0300

    Added solution for Sort Colors

--- Python_Basics(week1)/sort-colors.py
+++ Python_Basics(week1)/sort-colors.py
@@ -1,10 +1,22 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+class Solution(object):
+    def sortColors(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
-        for  j in range(1,len(nums)):
-            while j> 0 and nums[j] < nums[j-1]:
-                nums[j],nums[j-1] = nums[j-1],nums[j]
+        def swap(i,j):
+            nums[i],nums[j] = nums[j],nums[i]
+        k,j = 0 ,len(nums)-1
+        i = 0
+        while i<=j:
+            if nums[i] == 0:
+                swap(k,i)
+                k +=1
+            elif nums[i] == 2:
+                swap(i,j)
                 j -=1
+                i -=1
+                
+            i +=1
         return nums
+            
