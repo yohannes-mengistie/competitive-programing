1eac2da6bba53dfac28566ff3ee175c53301718c
yohannes-mengistie <120666489+yohannes-mengistie@users.noreply.github.com>
2024-01-10 19:21:36 +0300

    Add solution for MaxConsecutiveOnes

---
+++ Data structure first session/LeetCode/MaxConsecutiveOnes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        count_zero = 0
+        max_conscutive = 0
+        left = 0 
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                count_zero +=1
+            while count_zero > k:
+                if nums[left] == 0:
+                    count_zero -=1
+                left +=1
+            max_conscutive = max(max_conscutive,i-left+1)
+
+        return max_conscutive
