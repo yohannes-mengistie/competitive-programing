e384b34b93390070066e91bc4d9c108e2104f000
yohannes-mengistie <120666489+yohannes-mengistie@users.noreply.github.com>
2024-02-12 00:07:55 +0300

    Add solution for GoodSubarrays

---
+++ data structure first session/Codeforces/GoodSubarrays.py
@@ -0,0 +1,17 @@
+for _ in range(int(input())):
+    n = int(input())
+    arr = input()
+    
+    prefix = 0
+    dic = {0:1}
+    ans = 0
+    
+    for i in range(n):
+        prefix += int(arr[i])
+        x = prefix -  i -1
+        if x not in dic:
+            dic[x] = 0
+        dic[x] +=1
+        ans += dic[x] -1
+        
+    print(ans)
