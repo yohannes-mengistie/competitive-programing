f4b2ee94b5d5da55694d6874080fdedccd8823e8
yohannes-mengistie <120666489+yohannes-mengistie@users.noreply.github.com>
2024-02-22 18:04:46 +0300

    Add solution for Delete Node in a BST

---
+++ data structure first session/leetcode/delete-node-in-a-bst.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        if root.val > key:
+            root.left = self.deleteNode(root.left,key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right,key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            
+            else:
+                node = root.right
+                while node.left:
+                    
+                    node = node.left
+
+                
+                root.val = node.val
+                root.right = self.deleteNode(root.right,node.val)
+            
+
+        return root
+    
