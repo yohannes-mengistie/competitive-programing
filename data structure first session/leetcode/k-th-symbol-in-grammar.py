2e8263eb21d5b41c0bf361d46ce67eb180e4a8ef
yohannes-mengistie <120666489+yohannes-mengistie@users.noreply.github.com>
2024-03-01 18:56:24 +0300

    Add solution for K-th Symbol in Grammar

--- data structure first session/leetcode/k-th-symbol-in-grammar.py
+++ data structure first session/leetcode/k-th-symbol-in-grammar.py
@@ -5,11 +5,11 @@ class Solution:
 
         totalNodes = 2 ** (n - 1)
 
-        # Target node will be present in the right half sub-tree of the current root node.
+        
         if k > (totalNodes / 2):
             nextRootVal = 1 if rootVal == 0 else 0
             return self.depthFirstSearch(n - 1, k - (totalNodes / 2), nextRootVal)
-        # Otherwise, the target node is in the left sub-tree of the current root node.
+    
         else:
             nextRootVal = 0 if rootVal == 0 else 1
             return self.depthFirstSearch(n - 1, k, nextRootVal)
