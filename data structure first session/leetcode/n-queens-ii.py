3c5bd610e9b56bce4575da2150fec4bc45bec96b
yohannes-mengistie <120666489+yohannes-mengistie@users.noreply.github.com>
2024-03-01 23:09:23 +0300

    Add solution for N-Queens II

---
+++ data structure first session/leetcode/n-queens-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        col = set()
+        posdiag = set()
+        negdiag = set()
+        self.res = 0
+        board = [["."]*n for  i in range(n)]
+        def backtrack(r):
+            if r == n:
+               #copy = ["".join(row) for row in board]
+               self.res +=1
+               return
+            for c in range(n):
+                if c in col or (r+c) in posdiag or (r-c) in negdiag:
+                    continue
+                col.add(c)
+                posdiag.add(r+c)
+                negdiag.add(r-c)
+                board[r][c] = "Q"
+
+                backtrack(r+1)
+                col.remove(c)
+                posdiag.remove(r+c)
+                negdiag.remove(r-c)
+                board[r][c] = "."
+        backtrack(0)
+        return self.res
+
